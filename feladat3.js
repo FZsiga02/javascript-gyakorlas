214b804040761ecdb5df6900ad71f32363edf805
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-09-08 12:43:08 +0200

    Task 3

---
+++ feladat3.js
@@ -0,0 +1,16 @@
+function timer()
+{
+    console.log("Hurrá!")
+}
+
+setInterval(timer, 2000)
+
+let number=1;
+
+function count()
+{
+    console.log(number)
+    number++;
+}
+
+setInterval(count, 2000)
